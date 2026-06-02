a4cecbfd6336b87bb2b4734d9e94d5c29e3e8afe
Ivan Kohan <kohan3750@gmail.com>
2026-06-02 23:11:21 +0200

    change

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Contact } from "@/app/components/Contact";
 import { Footer } from "@/app/components/Footer";
 import { Process } from "@/app/components/Process";
 import { Testimonials } from "@/app/components/Testimonials";
-import { Faq } from "@/app/components/Faq";
+ 
 
 
 
